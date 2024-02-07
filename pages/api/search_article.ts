79bb821b4985bdd933a4d731cac0e049df7e19aa
Arief Zainuri <flaminglassoo1996@gmail.com>
2024-02-08 05:09:16 +0700

    Update data structure in search article

--- pages/api/search_article.ts
+++ pages/api/search_article.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { embeddings } from "../helpers/openai_instance";
 import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
 import { CharacterTextSplitter, RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import ArticleModel from "../modules/demo4/model/article_model";
+import ArticleModel, { ArticleChunkItem } from "../modules/demo4/model/article_model";
 import { article1, article2, article3, article4, article5, article6 } from "../../mock_article/articles";
 import { SearchArticleModel } from "./model/search_query_model";
 import { DocumentInterface } from "@langchain/core/documents";
@@ -35,26 +35,14 @@ export default async function handler(request: NextApiRequest, response: NextApi
         console.time('assignVectorStore');
         const vectorStore = await HNSWLib.fromDocuments(output, embeddings)
         console.timeEnd('assignVectorStore');
-        
+
         const articleList: ArticleModel[] = [
-            {
-                content: article1, similarityDocumentsCount: 0
-            },
-            {
-                content: article2, similarityDocumentsCount: 0
-            },
-            {
-                content: article3, similarityDocumentsCount: 0
-            },
-            {
-                content: article4, similarityDocumentsCount: 0
-            },
-            {
-                content: article5, similarityDocumentsCount: 0
-            },
-            {
-                content: article6, similarityDocumentsCount: 0
-            },
+            { content: article1 },
+            { content: article2 },
+            { content: article3 },
+            { content: article4 },
+            { content: article5 },
+            { content: article6 },
         ]
 
         for (const { index, value } of articleList.map((value, index) => ({ index, value }))) {
@@ -64,26 +52,43 @@ export default async function handler(request: NextApiRequest, response: NextApi
 
             console.log(`articleChunksLength ==> ${articleChunks.length}`)
 
-            let similarContent = 0
+            let similarDocumentCount = 0
+            let articleChunkList: ArticleChunkItem[] = []
 
             for (const value of articleChunks) {
                 console.time('similaritySearch')
                 const similarity = await vectorStore.similaritySearchWithScore(value.pageContent, 4)
                 console.timeEnd('similaritySearch')
-                
+
                 console.log(`\n\narticleChunk ==> ${value.pageContent}\n\n`)
                 similarity.forEach(element => {
                     console.log(`similarity ==> ${element[1]}`)
                 });
 
-                similarContent += similarity.filter(element => element[1] < 0.1).length
+                const _similarDocument = similarity.filter(element => element[1] < 0.1).map(element => element[0])
+                console.log(`_similarDocument ==> ${_similarDocument.length}`)
+
+                if (_similarDocument.length > 0) {
+                    similarDocumentCount += _similarDocument.length
+                    articleChunkList.push({
+                        chunk: value.pageContent,
+                        similarDocuments: _similarDocument
+                    })
+                    // articleList[index].chunkWithSimilarDocuments?.push()
+                }
             }
 
-            articleList[index].similarityDocumentsCount = similarContent
+            articleList[index].similarDocumentsCount = similarDocumentCount
+            articleList[index].chunkWithSimilarDocuments = articleChunkList
         }
 
         articleList.forEach(element => {
-            console.log(`articles ==> ${element.similarityDocumentsCount}`)
+            (element.chunkWithSimilarDocuments ?? []).forEach(_element => {
+                console.log(`\n\norigin chunk article ${_element.chunk}\n\n`)
+                _element.similarDocuments.forEach((_targetSimilar, index) => {
+                    console.log(`${index + 1}. ${_targetSimilar.pageContent}`)
+                });
+            });
         });
 
         response.status(200).json({
--- pages/modules/demo4/model/article_model.ts
+++ pages/modules/demo4/model/article_model.ts
@@ -1,4 +1,16 @@
+import { DocumentInterface } from "@langchain/core/documents";
+
 export default interface ArticleModel {
     content: string
-    similarityDocumentsCount: number
+    chunkWithSimilarDocuments?: ArticleChunkItem[]
+    similarDocumentsCount?: number
+    // // this is contains article target similar content
+    // similarDocuments?: DocumentInterface[]
+    // // this is contains article origin similar content
+    // similarOriginDocuments?: DocumentInterface[]
+}
+
+export interface ArticleChunkItem {
+    chunk: string
+    similarDocuments: DocumentInterface[]
 }
